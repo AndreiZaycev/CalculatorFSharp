4543f0e633dbbbf028d3f118a595992accfcdc42
AndreiZaycev <71140884+AndreiZaycev@users.noreply.github.com>
2021-04-21 15:52:55 +0300

    Update build.fsx

--- build.fsx
+++ build.fsx
@@ -94,7 +94,6 @@ let targetFramework =  "net5.0"
 // RuntimeIdentifiers: https://docs.microsoft.com/en-us/dotnet/core/rid-catalog
 // dotnet-packaging Tasks: https://github.com/qmfrederik/dotnet-packaging/blob/0c8e063ada5ba0de2b194cd3fad8308671b48092/Packaging.Targets/build/Packaging.Targets.targets
 let runtimes = [
-    "win-x64", "CreateZip"
 ]
 
 let disableCodeCoverage = environVarAsBoolOrDefault "DISABLE_COVERAGE" false
